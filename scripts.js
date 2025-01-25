0320de26d87380233fc565e09acca939d48cb48e
Erfan <khodami.erfan@gmail.com>
2025-01-25 18:40:52 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,4 @@
-// Get the toggle button and side menu
-const menuToggle = document.getElementById('menu-toggle');
-const sideMenu = document.getElementById('side-menu');
-
-// Add event listener for button click
-menuToggle.addEventListener('click', () => {
-    // Toggle the "active" class on the side menu
-    sideMenu.classList.toggle('active');
-});
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  sidebar.classList.toggle('active');
+}
