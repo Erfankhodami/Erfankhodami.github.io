e231f4469d7fb06dc0858bf26e5dddf907c1fea9
Erfankhodami <khodami.erfan@gmail.com>
2025-01-25 19:17:53 +0330

    commit2

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+let tapCount = 0;
+let timeout;
+
 function toggleSidebar() {
   const sidebar = document.getElementById('sidebar');
   sidebar.classList.toggle('active');
+
+  // Increment tap count for Pac-Man Easter egg
+  tapCount++;
+  clearTimeout(timeout);
+  timeout = setTimeout(() => (tapCount = 0), 1000);
+
+  if (tapCount === 5) {
+    displayPacMan();
+  }
+}
+
+function displayPacMan() {
+  const pacmanContainer = document.getElementById('pacman-container');
+  pacmanContainer.classList.remove('hidden');
 }
